7128e49b805b4171a73eff8de6e2162f42de710d
kouacc <maxrur57@gmail.com>
2025-02-11 11:17:07 +0100

    chore: configure content collections

---
+++ src/content.config.ts
@@ -0,0 +1,26 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from 'astro/loaders'
+
+const films = defineCollection({
+    loader: glob({base: "src/data/films", pattern: "**/*.md"}),
+    schema: z.object({
+        titre: z.string(),
+        date_sortie: z.date(),
+        synopsis: z.string(),
+        langue: z.string(),
+        duree: z.number(),
+    })
+})
+
+const personnes = defineCollection({
+  loader: glob({ base: "src/data/personnes", pattern: "**/*.md" }),
+  schema: z.object({
+    nom: z.string(),
+    dateNaissance: z.date(),
+    dateDeces: z.date().optional(),
+    nationalite: z.string(),
+    lieu_naissance: z.string(),
+  }),
+});
+
+export const collections = { personnes, films }
